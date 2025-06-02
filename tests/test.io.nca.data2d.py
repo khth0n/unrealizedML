49763d814f958837227d11b47745edad94390296
Isaiah C <isaiah.cordova21@gmail.com>
2025-06-01 23:33:10 -0500

    [TESTING]: initial io tests seem to work as expected for now.

--- tests/test.io.nca.data2d.py
+++ tests/test.io.nca.data2d.py
@@ -1,1 +1,25 @@
-from unrealizedML.io import nca
+from unrealizedML.io.nca.data2d import TrainingData2d, pickle_dir
+
+from pathlib import Path
+
+'''
+pickle_dir(
+    Path('test_data/voronoi_ed/raw/input'),
+    32,
+    Path('test_data/voronoi_ed/processed/input')
+)
+
+pickle_dir(
+    Path('test_data/voronoi_ed/raw/target'),
+    32,
+    Path('test_data/voronoi_ed/processed/target')
+)
+'''
+
+dataset = TrainingData2d(
+    Path('test_data/voronoi_ed/processed/input'),
+    Path('test_data/voronoi_ed/processed/target')
+)
+
+print(dataset[0][0].shape)
+
